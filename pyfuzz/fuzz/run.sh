ed32f0cb3d25fa1444336db3e68fc8581c769e5a
ksprsk <bchan052415@gmail.com>
2023-11-09 17:00:57 +0900

    fixed dir structure

--- pyfuzz/fuzz/run.sh
+++ pyfuzz/fuzz/run.sh
@@ -5,14 +5,14 @@ fi
 
 folder_name=$1
 cd $folder_name
-cp ../emulator.py . || exit
-cp -r ../data . 
-cd ../../AFLplusplus/unicorn_mode || exit
-cp -r ../../pyfuzz/$folder_name . || exit
+cp ../../emulator.py . || exit
+cp -r ../../data . 
+cd ../../../AFLplusplus/unicorn_mode || exit
+cp -r ../../pyfuzz/fuzz/$folder_name . || exit
 cd $folder_name || exit
 ../../afl-fuzz -U -m none -i ./"${folder_name}_input" -o ./"${folder_name}_output" -- python "${folder_name}.py" @@
-cp -r "${folder_name}_output" ../../../pyfuzz/$folder_name
+cp -r "${folder_name}_output" ../../../pyfuzz/fuzz/$folder_name
 cd ../
 rm -r $folder_name
-cd ../../pyfuzz/$folder_name
+cd ../../pyfuzz/fuzz/$folder_name
 rm -r data emulator.py
