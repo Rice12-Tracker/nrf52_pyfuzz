ad3269a1c5a2a5009c4c7ed6b02ae6d624cecfb3
ksprsk <bchan052415@gmail.com>
2023-11-16 14:30:07 +0900

    fixed build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install -y build-essential python3-dev automake cmake git flex bison libglib2.0-dev libpixman-1-dev python3-setuptools cargo libgtk-3-dev
+sudo apt-get install -y build-essential python3-dev automake cmake git flex bison libglib2.0-dev libpixman-1-dev python3-setuptools python3-pip cargo libgtk-3-dev
 # try to install llvm 14 and install the distro default if that fails
 sudo apt-get install -y lld-14 llvm-14 llvm-14-dev clang-14 || sudo apt-get install -y lld llvm llvm-dev clang
 sudo apt-get install -y gcc-$(gcc --version|head -n1|sed 's/\..*//'|sed 's/.* //')-plugin-dev libstdc++-$(gcc --version|head -n1|sed 's/\..*//'|sed 's/.* //')-dev
